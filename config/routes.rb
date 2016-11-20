5f05a53146b6165d7007d405b4ca8aad4b7b4fae
Cameron <cameron.barber.94@gmail.com>
2016-11-20 18:05:16 +1100

    Add jobs CRUD ability

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :jobs
+
+	get 'jobs/index'
+	get 'jobs/new'
+
+	root 'jobs#index'
 end
